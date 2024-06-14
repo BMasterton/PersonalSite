7f78de06053209dade74eaaf0ce1af133cd5f6c9
BMasterton <braonainmasterton@gmail.com>
2024-06-13 18:13:46 -0700

    Initial Homepage set up for later

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,5 +1,35 @@
 const Home = () => {
-    return <h1>Home</h1>;
+    return (
+        <div className="container m-0">
+            <h1>Home</h1>
+            <p> Welcome to the Portfolio of Braonain Masterton, Not all projects are listed here
+                and those that are not can be found on my github page. This is meant to quickly 
+                view of some projects i made over a couple of days. For more detailed games and projects
+                please check out my github for python games with custom server games, full web pages with 
+                front and back end, as well as IOS games and apps.
+            </p>
+
+            <br></br>
+            <p> please follow the links below if you wish to see some more indepth projects</p>
+
+            <p>link to github repos </p>
+            <ul>
+                <li><a href="url">Repo Link - Web Dev Page with backend</a> </li>
+                <li><a href="url">Repo Link - Apple IOS Game</a> </li>
+                <li><a href="url">Repo Link - Apple IOS other stuff</a> </li>
+                <li><a href="url">Repo Link - Python Game </a> </li>
+                <li><a href="url">Repo Link - C++ tetris</a> </li>
+                <li><a href="url">Repo Link - Jump Land </a> </li>
+                <li><a href="url">Repo Link - Contract View</a> </li>
+                <li><a href="url">Repo Link - AWS stuff</a> </li>
+            </ul>
+
+
+        </div>
+        
+       
+    
+    );
   };
   
   export default Home;
